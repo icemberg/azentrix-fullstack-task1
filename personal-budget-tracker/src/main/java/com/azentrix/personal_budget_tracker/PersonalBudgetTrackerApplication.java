5318fcba938a5164d11bfa1e8d8194538185f329
icemberg <abhiraams2004@gmail.com>
2026-06-14 11:34:21 +0530

    Project Initialized

---
+++ personal-budget-tracker/src/main/java/com/azentrix/personal_budget_tracker/PersonalBudgetTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.azentrix.personal_budget_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalBudgetTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalBudgetTrackerApplication.class, args);
+	}
+
+}
---
+++ personal-budget-tracker/src/test/java/com/azentrix/personal_budget_tracker/PersonalBudgetTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azentrix.personal_budget_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalBudgetTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
